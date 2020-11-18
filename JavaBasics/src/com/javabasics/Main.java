3cbf8294f2123bc77f3e66678e3b7fa5aace9c4c
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-18 10:44:32 -0700

    Create first project

---
+++ JavaBasics/src/com/javabasics/Main.java
@@ -0,0 +1,13 @@
+package com.javabasics;
+// Java statements should be terminated with a ;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // public meaning it is accessible from other parts of this program (it is global)
+        // Main method should always be static - static is a keyword that allows this class to be invoked without having to instantiate the class first
+        // The return type is void, which means that there is no return value for this method
+        // String[] args is the parameter for this method
+        System.out.println();
+    }
+}
