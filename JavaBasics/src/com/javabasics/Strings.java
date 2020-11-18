ec68b425857d2c443aa08ed8f9a8f17d040074dd
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-18 14:39:03 -0700

    Create String class

--- JavaBasics/src/com/javabasics/Strings.java
+++ JavaBasics/src/com/javabasics/Strings.java
@@ -1,2 +1,25 @@
-package com.javabasics;public class Strings {
+package com.javabasics;
+
+public class Strings {
+    public static void main (String[] args) {
+        String message1 = new String("Hello World");
+        String message2 = "Hello World";
+        // String types are reference types, so usually you have to declare a new instance of it
+        // But, java lets you do this the shorthand way as if you're declaring a primitive type - DON'T GET CONFUSED
+
+        // concat
+        System.out.println(message1.concat("!!!"));
+            // When you concat a string, the original stays the same
+        System.out.println(message1);
+
+        // endsWith
+        System.out.println(message1.endsWith("World"));
+
+        // startsWith
+        System.out.println(message1.endsWith("hello"));
+            // capitalization matters ^ this returns false
+
+        // length
+        System.out.println(message1.length());
+    }
 }
