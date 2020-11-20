204b53d6a4961d6bc0c3ccabaa7a9864c818410d
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-19 21:05:52 -0700

    Add if statement exercise

--- JavaBasics/src/com/flowControl/flowControl.java
+++ JavaBasics/src/com/flowControl/flowControl.java
@@ -1,2 +1,23 @@
-package com.flowControl;public class flowControl {
+package com.flowControl;
+
+public class flowControl {
+    public static void main(String[] args) {
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink some water");
+        } else if (temp > 20 && temp <=30) {
+            System.out.println("Beautiful day");
+        } else {
+            System.out.println("Cold day");
+        }
+
+        // clean one line boolean statement
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+
+        // ternary statement
+        String className = income >=100_000 ? "First" : "True";
+        System.out.println(className);
+    }
 }
