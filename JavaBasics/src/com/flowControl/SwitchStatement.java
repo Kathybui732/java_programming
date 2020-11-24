0c9e7c630e459848b9508a9c8d1507cc58206145
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-23 23:05:25 -0700

    Create switch statement exercise

--- JavaBasics/src/com/flowControl/SwitchStatement.java
+++ JavaBasics/src/com/flowControl/SwitchStatement.java
@@ -1,2 +1,27 @@
-package com.flowControl;public class SwitchStatement {
+package com.flowControl;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        String role = "admin";
+        if (role == "admin") {
+            System.out.println("You are an admin");
+        } else if (role == "moderator") {
+            System.out.println("You are a moderator");
+        } else {
+            System.out.println("You are a guest");
+        }
+
+        switch (role) {
+            case "admin":
+                System.out.println("You are an admin");
+                break;
+            case "moderator":
+                System.out.println("You are a moderator");
+                break;
+            default:
+                System.out.println("You are a guest");
+        }
+    }
 }
+
+// If a break statement isn't used, all cases with the matching conditions will be executed.
