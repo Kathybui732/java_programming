76122e8b02e4efe9be3e7267f1fb64a6e92a41b1
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-23 23:04:17 -0700

    Create for loop exercise

--- JavaBasics/src/com/flowControl/ForLoop.java
+++ JavaBasics/src/com/flowControl/ForLoop.java
@@ -1,6 +1,6 @@
 package com.flowControl;
 
-public class ForLoops {
+public class ForLoop {
     public static void main(String[] args) {
         int[] numbers = {3, 7, 5, -5};
 
@@ -9,3 +9,5 @@ public class ForLoops {
         }
     }
 }
+
+// we can also make infinite loops if we give it a condition that will never yield true
