ebcefa87603a28fec37d0713d207313725f4a783
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-23 23:05:11 -0700

    Create Loop exercise

--- JavaBasics/src/com/flowControl/Loop.java
+++ JavaBasics/src/com/flowControl/Loop.java
@@ -1,2 +1,23 @@
-package com.flowControl;public class Loops {
+package com.flowControl;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        // Normal loop
+        // for ( initialExpression; testExpression; updatedExpression) {
+        // Do this if test condition is true
+        // }
+
+        byte n = 5;
+        for (byte i = 0; i < n; ++i) {
+            System.out.println(i);
+        }
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
 }
