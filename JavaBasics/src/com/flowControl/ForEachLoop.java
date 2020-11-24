18742fd6bb6ef4a64d9f813215d56f2129dbe94b
Kathy Bui <kathy.bui1992@gmail.com>
2020-11-23 23:04:01 -0700

    Create for each loop exercise

--- JavaBasics/src/com/flowControl/ForEachLoop.java
+++ JavaBasics/src/com/flowControl/ForEachLoop.java
@@ -1,2 +1,19 @@
-package com.flowControl;public class ForEachLoop {
+package com.flowControl;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        // for(dataType item: array) {
+        // do this
+        // }
+
+        int[] numbers = {1,2,3,4};
+        int sum = 0;
+
+        for (int number:numbers) {
+            sum += number;
+        }
+        System.out.println(sum);
+    }
 }
+
+// for each loop is preferred over for loops when working with arrays/collections
